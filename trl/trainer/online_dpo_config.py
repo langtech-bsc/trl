0d9fdb6a95cb7cd16715e9fd8348e14f3a107fe9
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2025-06-13 13:40:12 +0200

    🎀 New defaults: `bf16=True` (#3515)

--- trl/trainer/online_dpo_config.py
+++ trl/trainer/online_dpo_config.py
@@ -87,6 +87,16 @@ class OnlineDPOConfig(TrainingArguments):
             )
         },
     )
+    bf16: bool = field(
+        default=True,
+        metadata={
+            "help": (
+                "Whether to use bf16 (mixed) precision instead of 32-bit. Requires Ampere or higher NVIDIA "
+                "architecture or using CPU (use_cpu) or Ascend NPU. This is an experimental API and it may change."
+            )
+        },
+    )
+
     ref_model_path: Optional[str] = field(
         default=None,
         metadata={
