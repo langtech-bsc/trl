3c64416dc0a85887c3e8764021f7b86cc2715cb5
Sergio Paniego Blanco <sergiopaniegoblanco@gmail.com>
2025-08-27 12:18:05 +0200

    Add HF jobs tag when creating model card via jobs (#3956)

--- trl/trainer/dpo_trainer.py
+++ trl/trainer/dpo_trainer.py
@@ -1983,6 +1983,9 @@ class DPOTrainer(Trainer):
         if hasattr(self.model.config, "unsloth_version"):
             tags.add("unsloth")
 
+        if "JOB_ID" in os.environ:
+            tags.add("hf_jobs")
+
         tags.update(self._tag_names)
 
         # docstyle-ignore
